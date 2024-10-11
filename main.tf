85f2f831886f2903a4e2a50c5b6106442630c4f7
ccie7599 <bapley@akamai.com>
2024-10-11 10:35:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,13 +115,15 @@ resource "null_resource" "generate_prometheus_targets" {
       output_file="${path.module}/prometheus_scrape_targets.yml"
       echo "- job_name: 'nats-server'" > $output_file
       echo "  static_configs:" >> $output_file
-      for ip in ${join(" ", flatten(linode_instance.linode[*].ipv4))}; do
+      ips="${join(" ", flatten(linode_instance.linode[*].ipv4))}"
+      for ip in $ips; do
         echo "    - targets: ['${ip}:7777']" >> $output_file
       done
     EOT
   }
   depends_on = [linode_instance.linode]
 }
+
 
 locals {
   ip_address = [for vm in linode_instance.linode : "${vm.ipv4}"]
