65ab223335dcb8427a3ea145f4427b097f6f2ba4
ccie7599 <bapley@akamai.com>
2024-10-11 10:33:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,14 +109,13 @@ output "all_ip_addresses_prom" {
   value = local.all_ip_addresses_prom
 }
 
-# Add the new resource to generate the Prometheus scrape targets
 resource "null_resource" "generate_prometheus_targets" {
   provisioner "local-exec" {
     command = <<EOT
       output_file="${path.module}/prometheus_scrape_targets.yml"
       echo "- job_name: 'nats-server'" > $output_file
       echo "  static_configs:" >> $output_file
-      for ip in ${join(" ", local.all_ip_addresses_prom)}; do
+      for ip in ${join(" ", flatten(linode_instance.linode[*].ipv4))}; do
         echo "    - targets: ['${ip}:7777']" >> $output_file
       done
     EOT
