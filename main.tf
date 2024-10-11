38f18f768776fded6ccaa8280685df4f302a427e
ccie7599 <bapley@akamai.com>
2024-10-11 10:25:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,30 @@ resource "linode_firewall" "nats_firewall" {
 output "ip_address" {
   value = [for vm in linode_instance.linode : "${vm.ipv4}"]                                                                                                                            
 }
-                                                                                                                                                                                       
+
+locals {
+  all_ip_addresses = [for vm in linode_instance.linode : "${vm.ipv4}"]
+}
+
+output "all_ip_addresses" {
+  value = local.all_ip_addresses
+}
+
+# Add the new resource to generate the Prometheus scrape targets
+resource "null_resource" "generate_prometheus_targets" {
+  provisioner "local-exec" {
+    command = <<EOT
+      output_file="${path.module}/prometheus_scrape_targets.yml"
+      echo "- job_name: 'nats-server'" > $output_file
+      echo "  static_configs:" >> $output_file
+      for ip in ${join(" ", local.all_ip_addresses)}; do
+        echo "    - targets: ['${ip}:7777']" >> $output_file
+      done
+    EOT
+  }
+  depends_on = [linode_instance.linode]
+}
+
 locals {
   ip_address = [for vm in linode_instance.linode : "${vm.ipv4}"]
   jp_osa_ip_address = [for vm in linode_instance.linode : vm.ipv4 if vm.region == "jp-osa"]
